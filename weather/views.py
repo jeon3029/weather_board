6c5f673f1b8dbb59d587ffab29e4341d315a517c
taejoonJeon <taejjeon@gmail.com>
2020-06-11 16:25:40 +0900

    add git ignore

--- weather/views.py
+++ weather/views.py
@@ -3,12 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 import requests
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'index2.html')
     city = request.GET.get('city')
     # city = 'seoul'
     print(city)
-    url = 'http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=bc93e91cf9c28d68d266b93b2e7a46da'
-    resp = requests.get(url=url,params=dict())
-    data = resp.json()
-    return JsonResponse(data)
-    # return JsonResponse({'key':'value'})
